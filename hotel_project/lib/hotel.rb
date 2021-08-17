663d042c6b5241d644c7280ef29e496f258eaad8
Tristan Angeles <setswipe@gmail.com>
2021-08-17 16:32:08 -0400

    startup and employee projects added (completed)

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,59 @@
 require_relative "room"
+require "byebug"
 
 class Hotel
+
+  attr_accessor :rooms
   
-end
+  def initialize (name, hsh)
+
+    @name = name
+    @rooms = {}
+    hsh.each do |k, v| 
+      @rooms[k] = Room.new(v)
+    end
+  end
+
+  def name
+    words=@name.split(" ")
+    words.map {|word| word[0]=word[0].upcase}
+    
+    words.join(" ")
+  end
+
+  def room_exists? (str)
+    @rooms.include?(str)
+  end
+
+  def check_in (str, roomName)
+    if !room_exists? (roomName)
+      puts 'sorry, room does not exist'
+    else 
+      if @rooms[roomName].add_occupant(roomName)
+        puts 'check in successful'
+      else
+        puts 'sorry, room is full'
+      end
+    end
+  end
+
+  def has_vacancy?
+    if @rooms.any? {|k, v| !v.full?}
+      return true
+    end
+
+    if @rooms.none? {|k, v| v.full?}
+      return false
+    end
+    false
+
+  end
+
+  def list_rooms
+    @rooms.keys.each do |rName|
+      puts rName + @rooms[rName].available_space.to_s
+    end
+  end
+
+
+end # end Hotel Class
--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,86 @@
 require "employee"
 
 class Startup
+attr_reader :name, :funding, :salaries, :employees
 
+def initialize(name, funding, salaries)
+  @name = name
+  @funding = funding
+  @salaries  = salaries
+  @employees = []
 end
+
+def valid_title?(str)
+  if @salaries.has_key?(str)
+    return true
+  end
+  false
+end
+
+def >(startup)
+  if self.funding>startup.funding
+    return true
+  end
+  false
+
+  
+end
+
+def hire(name, title)
+  if !valid_title?(title)
+    raise error
+  end
+
+  @employees << Employee.new(name, title)
+end
+
+def size
+  employees.length
+end
+
+def pay_employee(dude)
+  salary = @salaries[dude.title]
+  if @funding-salary<=0
+    raise error
+  else
+    dude.pay(salary)
+    @funding -= salary 
+  end
+end
+
+def payday
+  @employees.each {|employee| self.pay_employee(employee)}
+end
+
+#-------------part 3-------------
+
+def average_salary
+  sum = 0.0
+  employees.each do |dude| 
+    sum += salaries[dude.title]
+  end
+  sum /employees.length
+end
+
+def close
+  @employees = []
+  @funding = 0
+end
+
+def acquire (startup)
+  @funding += startup.funding
+  startup.salaries.each do | k, v|
+    if !@salaries.has_key?(k)
+      @salaries[k] = v
+    end
+  end
+
+  startup.employees.each do |dude|
+    @employees << dude
+  end
+
+  startup.close
+end
+
+
+end #end Startup Class
